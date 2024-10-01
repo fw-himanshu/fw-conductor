ce2644f196dfe5e5b9960ec9ac969d10e8ff471b
Himanshu Sharma <himanshu.sharma@freshworks.com>
2024-10-01 16:03:17 +0530

    Fixed SQS issue

--- awssqs-event-queue/src/main/java/com/netflix/conductor/sqs/dao/SQSQueueDAO.java
+++ awssqs-event-queue/src/main/java/com/netflix/conductor/sqs/dao/SQSQueueDAO.java
@@ -63,6 +63,7 @@ public class SQSQueueDAO implements QueueDAO {
         SendMessageRequest request =
                 new SendMessageRequest()
                         .withQueueUrl(getQueueUrl(queueName))
+                        .withMessageBody(id)
                         .withDelaySeconds((int) offsetTimeInSecond);
         sqsClient.sendMessage(request);
     }
